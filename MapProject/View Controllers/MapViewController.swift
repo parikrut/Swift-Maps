c7f0604aa45409ada2520a18223026c1a62dad96
Xcode User <xcode@Fasts-MacBook-Pro-2.local>
2020-11-04 23:48:49 -0500

    Initial

---
+++ MapProject/View Controllers/MapViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MapViewController.swift
+//  MapProject
+//
+//  Created by Xcode User on 2020-10-07.
+//  Copyright © 2020 Xcode User. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MapProject/View Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapProject
+//
+//  Created by Xcode User on 2020-10-07.
+//  Copyright © 2020 Xcode User. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
